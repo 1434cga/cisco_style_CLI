dae43ca79c0c50bb98b6800ad70c5fd4109963d8
Charles.Lee <cheoljoo.lee@lge.com>
2019-06-07 14:45:20 +0900

    cli.cpp : divide into two file (cli.cpp and cli.h) - cli.cpp : include cli.h   + logic - cli.h :  input data structure

--- cli.cpp
+++ cli.cpp
@@ -77,121 +77,9 @@ vector<string> vectorHistory;
 
 
 /*
-vectorArg testarg = {
-                    { "name-string", "description string for A" , ArgumentType::String}, // argument 1
-                    { "name-int", "description integer for A" , ArgumentType::Int}, // argument 2
-                    { "name-hexa", "description hexa for A" , ArgumentType::Hex}, // argument 3
-};
-
-mapApi testapi = {
-    { // mapApi map
-        {"wifi" , "wifi is our first module" , ArgumentType::None }, // Argument
-        {   // vector<Argument> = vectorArg
-            { "name-string", "description string for A" , ArgumentType::String}, // argument 1
-            { "name-int", "description integer for A" , ArgumentType::Int}, // argument 2
-            { "name-hexa", "description hexa for A" , ArgumentType::Hex}, // argument 3
-        }
-    },
-};
+ * define data structure for input
 */
-
-
-mapMod rootmod = {       // mapMod
-    {       // mapMod map <Argument,mapApi>
-        {"mode" , "show the mode in Normal / Privileged " , ArgumentType::None , ModeType::Normal }, // Argument
-        {   // mapApi
-            { // mapApi map
-            },
-        }
-    },
-    {       // mapMod map <Argument,mapApi>
-        {"version" , "version" , ArgumentType::None , ModeType::Normal }, // Argument
-        {   // mapApi
-            { // mapApi map
-            },
-        }
-    },
-    {       // mapMod map <Argument,mapApi>
-        {"quit" , "quit program" , ArgumentType::None , ModeType::Normal }, // Argument
-        {   // mapApi
-            { // mapApi map
-            },
-        }
-    },
-    {       // mapMod map <Argument,mapApi>
-        {"list" , "show all command lists" , ArgumentType::None , ModeType::Normal }, // Argument
-        {   // mapApi
-            { // mapApi map
-            },
-        }
-    },
-    {       // mapMod map <Argument,mapApi>
-        {"help" , "show all command lists" , ArgumentType::None , ModeType::Normal }, // Argument
-        {   // mapApi
-            { // mapApi map
-            },
-        }
-    },
-    {       // mapMod map <Argument,mapApi>
-        {"history" , "history" , ArgumentType::None , ModeType::Normal }, // Argument
-        {   // mapApi
-            { // mapApi map
-            },
-        }
-    },
-    {       // mapMod map <Argument,mapApi>
-        {"testwifi" , "wifi is our first module" , ArgumentType::None , ModeType::Normal }, // Argument
-        {   // mapApi
-            { // mapApi map
-                {"wifi-api1" , "wifi-api1 is our first module" , ArgumentType::None , ModeType::Normal }, // Argument
-                {   // vectorArg
-                    { "name-string", "description string for A" , ArgumentType::String, ModeType::Normal }, // argument 1
-                    { "name-int", "description integer for A" , ArgumentType::Int, ModeType::Normal }, // argument 2
-                    { "name-hexa", "description hexa for A" , ArgumentType::Hex, ModeType::Normal }, // argument 3
-                }
-            },
-            { // mapApi map
-                {"api-privileged" , "wifi-api2 is our first module" , ArgumentType::None , ModeType::Privileged }, // Argument
-                {   // vectorArg
-                    { "name-string", "description string for A" , ArgumentType::String, ModeType::Normal }, // argument 1
-                    { "name-hexa", "description hexa for A" , ArgumentType::Hex, ModeType::Normal }, // argument 2
-                }
-            },
-            { // mapApi map
-                {"wapi-api2" , "wifi-api2 is our first module" , ArgumentType::None , ModeType::Privileged }, // Argument
-                {   // vectorArg
-                    { "name-string", "description string for A" , ArgumentType::String, ModeType::Normal }, // argument 1
-                    { "name-hexa", "description hexa for A" , ArgumentType::Hex, ModeType::Normal }, // argument 2
-                }
-            },
-            { // mapApi map
-                {"wifi-api3" , "wifi-api3 is our first module" , ArgumentType::None , ModeType::Normal }, // Argument
-                {   // vectorArg
-                }
-            },
-            { // mapApi map
-                {"wifi-api4" , "wifi-api4 is our first module" , ArgumentType::None , ModeType::Privileged }, // Argument
-                {   // vectorArg
-                }
-            },
-        }
-    },
-    {       // mapMod map <Argument,mapApi>
-        {"testdongle" , "dongle is our first module" , ArgumentType::None , ModeType::Normal }, // Argument
-        {   // mapApi
-            { // mapApi map
-                {"dongle-api" , "dongle-api is our first module" , ArgumentType::None , ModeType::Normal }, // Argument
-                {   // vectorArg
-                    { "dongle-string", "description string for A" , ArgumentType::String, ModeType::Normal }, // argument 1
-                    { "dongle-int", "description integer for A" , ArgumentType::Int, ModeType::Normal }, // argument 2
-                    { "dongle-hexa", "description hexa for A" , ArgumentType::Hex, ModeType::Normal }, // argument 3
-                    { "dongle-hexa", "description hexa for A" , ArgumentType::Hex, ModeType::Normal }, // argument 3
-                    { "dongle-int", "description integer for A" , ArgumentType::Int, ModeType::Normal }, // argument 5
-                }
-            },
-        }
-    },
-};
+#include "cli.h"
 
 
 bool
@@ -637,6 +525,7 @@ loop(string& s)
     if(strToken[0] == "mode"){ show_mode(); }
 
     vectorHistory.push_back(hs);
+
 
     return 1;
 }
--- cli.h
+++ cli.h
@@ -1,76 +1,119 @@
-using namespace std;
 
 
-enum class ArgumentType {
-    None,
-    String,
-    Int,
-    Hex,
-    Ip,
-};
-
-class Argument {
-public:
-    string name;
-    string desc;
-    ArgumentType type;
-    Argument(string n , string d, ArgumentType t) : name(n),desc(d),type(t){}
-    ~Argument() {}
-    bool operator< (const Argument& userObj) const
-    {
-        return userObj.name > this->name;
-    }
-};
-
-using vectorArg = vector<Argument>;
-using mapApi = map<Argument,vectorArg>;
-using mapMod = map<Argument, mapApi >;
-vector<string> vectorHistory;
-
-
-string command = "sldd";
-
-mapMod rootmod = {       // mapMod
-    {       // mapMod map <Argument,mapApi>
-        {"wifi" , "wifi is our first module" , ArgumentType::None }, // Argument
-        {   // mapApi
-            { // mapApi map
-                {"wifi-api1" , "wifi-api1 is our first module" , ArgumentType::None }, // Argument
-                {   // vectorArg
+/*
+vectorArg testarg = {
                     { "name-string", "description string for A" , ArgumentType::String}, // argument 1
                     { "name-int", "description integer for A" , ArgumentType::Int}, // argument 2
                     { "name-hexa", "description hexa for A" , ArgumentType::Hex}, // argument 3
-                }
-            },
+};
+
+mapApi testapi = {
+    { // mapApi map
+        {"wifi" , "wifi is our first module" , ArgumentType::None }, // Argument
+        {   // vector<Argument> = vectorArg
+            { "name-string", "description string for A" , ArgumentType::String}, // argument 1
+            { "name-int", "description integer for A" , ArgumentType::Int}, // argument 2
+            { "name-hexa", "description hexa for A" , ArgumentType::Hex}, // argument 3
+        }
+    },
+};
+*/
+
+
+mapMod rootmod = {       // mapMod
+    {       // mapMod map <Argument,mapApi>
+        {"mode" , "show the mode in Normal / Privileged " , ArgumentType::None , ModeType::Normal }, // Argument
+        {   // mapApi
             { // mapApi map
-                {"wapi-api2" , "wifi-api2 is our first module" , ArgumentType::None }, // Argument
+            },
+        }
+    },
+    {       // mapMod map <Argument,mapApi>
+        {"version" , "version" , ArgumentType::None , ModeType::Normal }, // Argument
+        {   // mapApi
+            { // mapApi map
+            },
+        }
+    },
+    {       // mapMod map <Argument,mapApi>
+        {"quit" , "quit program" , ArgumentType::None , ModeType::Normal }, // Argument
+        {   // mapApi
+            { // mapApi map
+            },
+        }
+    },
+    {       // mapMod map <Argument,mapApi>
+        {"list" , "show all command lists" , ArgumentType::None , ModeType::Normal }, // Argument
+        {   // mapApi
+            { // mapApi map
+            },
+        }
+    },
+    {       // mapMod map <Argument,mapApi>
+        {"help" , "show all command lists" , ArgumentType::None , ModeType::Normal }, // Argument
+        {   // mapApi
+            { // mapApi map
+            },
+        }
+    },
+    {       // mapMod map <Argument,mapApi>
+        {"history" , "history" , ArgumentType::None , ModeType::Normal }, // Argument
+        {   // mapApi
+            { // mapApi map
+            },
+        }
+    },
+    {       // mapMod map <Argument,mapApi>
+        {"testwifi" , "wifi is our first module" , ArgumentType::None , ModeType::Normal }, // Argument
+        {   // mapApi
+            { // mapApi map
+                {"wifi-api1" , "wifi-api1 is our first module" , ArgumentType::None , ModeType::Normal }, // Argument
                 {   // vectorArg
-                    { "name-string", "description string for A" , ArgumentType::String}, // argument 1
-                    { "name-hexa", "description hexa for A" , ArgumentType::Hex}, // argument 2
+                    { "name-string", "description string for A" , ArgumentType::String, ModeType::Normal }, // argument 1
+                    { "name-int", "description integer for A" , ArgumentType::Int, ModeType::Normal }, // argument 2
+                    { "name-hexa", "description hexa for A" , ArgumentType::Hex, ModeType::Normal }, // argument 3
                 }
             },
             { // mapApi map
-                {"wifi-api3" , "wifi-api3 is our first module" , ArgumentType::None }, // Argument
+                {"api-privileged" , "wifi-api2 is our first module" , ArgumentType::None , ModeType::Privileged }, // Argument
+                {   // vectorArg
+                    { "name-string", "description string for A" , ArgumentType::String, ModeType::Normal }, // argument 1
+                    { "name-hexa", "description hexa for A" , ArgumentType::Hex, ModeType::Normal }, // argument 2
+                }
+            },
+            { // mapApi map
+                {"wapi-api2" , "wifi-api2 is our first module" , ArgumentType::None , ModeType::Privileged }, // Argument
+                {   // vectorArg
+                    { "name-string", "description string for A" , ArgumentType::String, ModeType::Normal }, // argument 1
+                    { "name-hexa", "description hexa for A" , ArgumentType::Hex, ModeType::Normal }, // argument 2
+                }
+            },
+            { // mapApi map
+                {"wifi-api3" , "wifi-api3 is our first module" , ArgumentType::None , ModeType::Normal }, // Argument
+                {   // vectorArg
+                }
+            },
+            { // mapApi map
+                {"wifi-api4" , "wifi-api4 is our first module" , ArgumentType::None , ModeType::Privileged }, // Argument
                 {   // vectorArg
                 }
             },
         }
     },
     {       // mapMod map <Argument,mapApi>
-        {"dongle" , "dongle is our first module" , ArgumentType::None }, // Argument
+        {"testdongle" , "dongle is our first module" , ArgumentType::None , ModeType::Normal }, // Argument
         {   // mapApi
             { // mapApi map
-                {"dongle-api" , "dongle-api is our first module" , ArgumentType::None }, // Argument
+                {"dongle-api" , "dongle-api is our first module" , ArgumentType::None , ModeType::Normal }, // Argument
                 {   // vectorArg
-                    { "dongle-string", "description string for A" , ArgumentType::String}, // argument 1
-                    { "dongle-int", "description integer for A" , ArgumentType::Int}, // argument 2
-                    { "dongle-hexa", "description hexa for A" , ArgumentType::Hex}, // argument 3
-                    { "dongle-hexa", "description hexa for A" , ArgumentType::Hex}, // argument 3
-                    { "dongle-int", "description integer for A" , ArgumentType::Int}, // argument 5
+                    { "dongle-string", "description string for A" , ArgumentType::String, ModeType::Normal }, // argument 1
+                    { "dongle-int", "description integer for A" , ArgumentType::Int, ModeType::Normal }, // argument 2
+                    { "dongle-hexa", "description hexa for A" , ArgumentType::Hex, ModeType::Normal }, // argument 3
+                    { "dongle-hexa", "description hexa for A" , ArgumentType::Hex, ModeType::Normal }, // argument 3
+                    { "dongle-int", "description integer for A" , ArgumentType::Int, ModeType::Normal }, // argument 5
                 }
             },
         }
     },
 };
-
 
